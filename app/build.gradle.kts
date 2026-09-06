74e30eae19f9017684694f1ba4960bf82cbe6ad3
kendekallom-afk <kendekallo.m@gmail.com>
2026-09-07 03:02:00 +0800

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.mka.navbar"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.mka.navbar"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
